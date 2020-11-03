449f8116c0707809110be1f9b960bbb6a5bb6536
José Alberto Espinoza Morelos <spartan73rmy12@gmail.com>
2020-11-03 16:29:44 -0600

    Deploy

---
+++ FitoReport.Domain/Entities/ArchivoReporte.cs
@@ -0,0 +1,15 @@
+﻿namespace FitoReport.Domain.Entities
+{
+    public class ArchivoReporte : BaseEntity
+    {
+        public ArchivoReporte()
+        {
+        }
+        public int IdUsuario { get; set; }
+        public int IdReporte { get; set; }
+        public int? IdArchivo { get; set; }
+
+        public virtual ArchivoUsuario Archivo { get; set; }
+        public virtual Usuario IdUsuarioNavigation { get; set; }
+    }
+}
